d77aa08ce5ca8c992cd054a2e28d283851a52d67
adamdebalke <youngadam65@gmail.com>
2022-05-14 19:00:20 +0300

    fix interface

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from flask import render_template, request
 from flask import redirect, url_for
+import os
 
-
+UPLOAD_FLODER = 'static/uploads'
 def base():
     return render_template('base.html')
+
+
+
+
+
+
+
